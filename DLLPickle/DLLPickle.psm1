e8ad09fdd878caf4ba9f39719b5769c91a2b5e61
Sam Erde <samuel.erde@gmail.com>
2025-10-22 05:23:17 -0400

    feat: automatically load the assembly on module import and attempt to remove it when the module is removed.

--- DLLPickle/DLLPickle.psm1
+++ DLLPickle/DLLPickle.psm1
@@ -1,12 +1,27 @@
-# Dot source public/private functions
-$public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
-foreach ($import in @($public + $private)) {
+# Dot-source public/private functions.
+$Public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
+$Private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
+foreach ($Import in @($Public + $Private)) {
     try {
-        . $import.FullName
+        . $Import.FullName
     } catch {
-        throw "Unable to dot source [$($import.FullName)]"
+        throw "Unable to dot source [$($Import.FullName)]"
     }
 }
 
-Export-ModuleMember -Function $public.Basename
+Export-ModuleMember -Function $Public.Basename
+
+# Load the assembly on module import. In PowerShell 7+, use Assembly Load Context (ALC) to avoid conflicts.
+    if ($PSVersionTable.PSVersion.Major -ge 7) {
+        Add-MsalAssembly
+    } else {
+        $MsalDllPath = Join-Path $PSScriptRoot 'lib\Microsoft.Identity.Client.dll'
+        Add-Type -Path $MsalDllPath
+    }
+
+# Unload the assembly on module removal (ALC requires PS 7 or higher).
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
+    if ($PSVersionTable.PSVersion.Major -ge 7) {
+        Remove-MsalAssembly
+    }
+}
