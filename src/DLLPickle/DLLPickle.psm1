11eb037563be37a692e9b97e5a577e1e52a0ec40
Sam Erde <samuel.erde@gmail.com>
2025-11-15 15:37:43 -0500

    Zoom and enhance. Change CI workflow to only run on manual trigger for now.

--- src/DLLPickle/DLLPickle.psm1
+++ src/DLLPickle/DLLPickle.psm1
@@ -1,34 +1,31 @@
-# this psm1 is for local testing and development use only
+# This PSM1 is for local testing and development use only.
 
-# dot source the parent import for local development variables
+# Dot-source the parent import for local development variables.
 . $PSScriptRoot\Imports.ps1
 
-# discover all ps1 file(s) in Public and Private paths
-
-$itemSplat = @{
+# Discover all PS1 file(s) in Public and Private paths.
+$ItemSplat = @{
     Filter      = '*.ps1'
     Recurse     = $true
     ErrorAction = 'Stop'
 }
 try {
-    $public = @(Get-ChildItem -Path "$PSScriptRoot\Public" @itemSplat)
-    $private = @(Get-ChildItem -Path "$PSScriptRoot\Private" @itemSplat)
-    $classes = @(Get-ChildItem -Path "$PSScriptRoot\Classes" @itemSplat)
-}
-catch {
+    $Public = @(Get-ChildItem -Path "$PSScriptRoot\Public" @ItemSplat)
+    $Private = @(Get-ChildItem -Path "$PSScriptRoot\Private" @ItemSplat)
+    #$Classes = @(Get-ChildItem -Path "$PSScriptRoot\Classes" @itemSplat)
+} catch {
     Write-Error $_
     throw 'Unable to get get file information from Public/Private/Classes src.'
 }
 
-# dot source all .ps1 file(s) found
-foreach ($file in @($public + $private + $classes)) {
+# Dot-source all .ps1 file(s) found.
+foreach ($File in @($Public + $Private)) {
     try {
-        . $file.FullName
-    }
-    catch {
-        throw ('Unable to dot source {0}' -f $file.FullName)
+        . $File.FullName
+    } catch {
+        throw ('Unable to dot source {0}' -f $File.FullName)
     }
 }
 
-# export all public functions
-Export-ModuleMember -Function $public.Basename
+# Export all public functions.
+Export-ModuleMember -Function $Public.Basename
--- src/DLLPickle/Imports.ps1
+++ src/DLLPickle/Imports.ps1
@@ -2,5 +2,5 @@
 # It can be imported by the psm1 in local development to add script level variables.
 # It will merged in the build process. This is for local development only.
 
-# region script variables
-# $script:resourcePath = "$PSScriptRoot\Resources"
+# region Script Variables
+# $script:ResourcePath = "$PSScriptRoot\Resources"
