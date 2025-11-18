fb1e607074bc52ba5aae46c6eacece4e7d2ac89f
Sam Erde <samuel.erde@gmail.com>
2025-11-18 09:03:16 -0500

    fix: Correct indentation in Find-MSIdentityAssembly documentation

--- src/DLLPickle/Public/Find-MSIdentityAssembly.ps1
+++ src/DLLPickle/Public/Find-MSIdentityAssembly.ps1
@@ -1,39 +1,39 @@
 ﻿function Find-MSIdentityAssembly {
-<#
-.SYNOPSIS
-    Show a list of all files in the current location that have 'Microsoft Identity' in their product name.
+    <#
+    .SYNOPSIS
+        Show a list of all files in the current location that have 'Microsoft Identity' in their product name.
 
-.DESCRIPTION
-    Check all installed PowerShell locations for DLL files that have 'Microsoft Identity' in their file's Productname attribute. (Can optionally check other locations.)
+    .DESCRIPTION
+        Check all installed PowerShell locations for DLL files that have 'Microsoft Identity' in their file's Productname attribute. (Can optionally check other locations.)
 
-.EXAMPLE
-    Find-MSIdentityAssembly
+    .EXAMPLE
+        Find-MSIdentityAssembly
 
-    Find all Microsoft Identity-related DLLs within installed PowerShell module locations.
+        Find all Microsoft Identity-related DLLs within installed PowerShell module locations.
 
-.EXAMPLE
-    Find-MSIdentityAssembly | Format-Table InternalName, @{Label = 'ProductVersion'; Expression = { $_.ProductVersionRaw } }, @{Label = 'Module'; Expression = { $($_.FileName -replace '^.*Modules[\\/]([^\\/]+)([\\/].*)?', '$1') }}
+    .EXAMPLE
+        Find-MSIdentityAssembly | Format-Table InternalName, @{Label = 'ProductVersion'; Expression = { $_.ProductVersionRaw } }, @{Label = 'Module'; Expression = { $($_.FileName -replace '^.*Modules[\\/]([^\\/]+)([\\/].*)?', '$1') }}
 
-    Find all Microsoft Identity-related DLLs within installed PowerShell module locations. Shows the name of the module that the DLL is included in.
+        Find all Microsoft Identity-related DLLs within installed PowerShell module locations. Shows the name of the module that the DLL is included in.
 
-.NOTES
+    .NOTES
 
-    To do: add informational output if no libraries are found.
+        To do: add informational output if no libraries are found.
 
-    Example Output:
+        Example Output:
 
-    InternalName                                        ProductVersion Module
-    ------------                                        -------------- ------
-    Microsoft.Identity.Abstractions.dll                 9.5.0.0        DLLPickle
-    Microsoft.IdentityModel.Abstractions.dll            0.0.0.0        Az.Accounts
-    Microsoft.IdentityModel.JsonWebTokens.dll           8.6.0.0        ExchangeOnlineManagement
-    Microsoft.IdentityModel.Logging.dll                 8.6.0.0        ExchangeOnlineManagement
-    Microsoft.IdentityModel.Protocols.dll               8.6.1.0        WinTuner
-    Microsoft.IdentityModel.Protocols.OpenIdConnect.dll 8.6.1.0        WinTuner
-    Microsoft.IdentityModel.Tokens.dll                  8.6.0.0        ExchangeOnlineManagement
-    Microsoft.IdentityModel.Validators.dll              8.6.1.0        WinTuner
-    System.IdentityModel.Tokens.Jwt.dll                 8.6.0.0        ExchangeOnlineManagement
-#>
+        InternalName                                        ProductVersion Module
+        ------------                                        -------------- ------
+        Microsoft.Identity.Abstractions.dll                 9.5.0.0        DLLPickle
+        Microsoft.IdentityModel.Abstractions.dll            0.0.0.0        Az.Accounts
+        Microsoft.IdentityModel.JsonWebTokens.dll           8.6.0.0        ExchangeOnlineManagement
+        Microsoft.IdentityModel.Logging.dll                 8.6.0.0        ExchangeOnlineManagement
+        Microsoft.IdentityModel.Protocols.dll               8.6.1.0        WinTuner
+        Microsoft.IdentityModel.Protocols.OpenIdConnect.dll 8.6.1.0        WinTuner
+        Microsoft.IdentityModel.Tokens.dll                  8.6.0.0        ExchangeOnlineManagement
+        Microsoft.IdentityModel.Validators.dll              8.6.1.0        WinTuner
+        System.IdentityModel.Tokens.Jwt.dll                 8.6.0.0        ExchangeOnlineManagement
+    #>
 
     [CmdletBinding()]
     param (
