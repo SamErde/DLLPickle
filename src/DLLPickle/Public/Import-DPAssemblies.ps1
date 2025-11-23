f40a5d067cee9fa9704cfce4cb3ea2d743f16825
Sam Erde <samuel.erde@gmail.com>
2025-11-22 23:57:07 -0500

    Add Import-DPAssembly function to import DLLs from specified packages

---
+++ src/DLLPickle/Public/Import-DPAssemblies.ps1
@@ -0,0 +1,36 @@
+﻿function Import-DPAssembly {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+
+    [CmdletBinding()]
+    param (
+        # Ignore preset 'autoImport' values and attempt to import all packages.
+        [Parameter()]
+        [switch] $ImportAll
+    )
+
+    $ModuleDirectory = if ($PSModuleRoot) { $PSModuleRoot } elseif ($PSScriptRoot) { Split-Path -Path $PSScriptRoot -Parent } else { $PWD }
+    $AssemblyDirectory = Join-Path -Path $ModuleDirectory -ChildPath 'Assembly'
+    $Packages = Get-Content -Path (Join-Path -Path $AssemblyDirectory -ChildPath 'Packages.json') |
+        ConvertFrom-Json | Select-Object -ExpandProperty packages
+
+    foreach ( $Package in $Packages) {
+        $FilePath = Join-Path -Path $AssemblyDirectory -ChildPath "$($Package.name).dll"
+        if ( $Package.autoImport -eq $true -or $PSBoundParameters.ContainsKey('ImportAll') ) {
+            Add-Type -Path $FilePath
+        } else {
+            Write-Verbose "Skipping auto-import for $FilePath."
+        }
+    }
+}
