b3de82fff26c2c1c91d5cf59587f4288e41ef6c8
Sam Erde <20478745+SamErde@users.noreply.github.com>
2026-03-08 07:54:49 -0400

    chore: Remove draft snippets

--- src/Drafts/Settings.ps1
+++
@@ -1,7 +0,0 @@
-if ($Settings.ShowLogo) {
-    Write-Host 'DLLPickle v1.0 - Picking DLLs with style!' -ForegroundColor Cyan
-}
-
-if ($Settings.CheckForUpdates) {
-    # Logic to check GitHub/PSGallery
-}
